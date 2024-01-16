02104b9be3113251a9556efcadf55d4272067aad
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-01-16 10:49:23 +0530

    feat: implement the feature to convert currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import {InputBox} from "./components";
+import { useState } from "react";
+import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  const currencyInfo = useCurrencyInfo("inr");
-  console.log(currencyInfo);
+  const [amount, setAmount] = useState(0);
+  const [fromValue, setFromValue] = useState("usd");
+  const [toValue, setToValue] = useState("inr");
+  const [result, setResult] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(fromValue);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFromValue(toValue);
+    setToValue(fromValue);
+    setResult(amount);
+    setAmount(result);
+  };
+
+  const convert = () => {
+    setResult(amount * currencyInfo[toValue]);
+  };
+
   return (
     <div
       className=" w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
@@ -16,28 +35,44 @@ function App() {
           <form
             onSubmit={(e) => {
               e.preventDefault();
+              convert();
             }}
           >
             <div className="w-full mb-1">
-              <InputBox />
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                selectCurrency={fromValue}
+                onCurrencyChange={(currency) => setFromValue(currency)}
+                onAmountChange={(amount) => setAmount(amount)}
+              />
             </div>
             <div className="relative w-full h-0.5">
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-500"
+                onClick={swap}
               >
                 Swap
               </button>
             </div>
 
             <div className="w-full mt-1 mb-4">
-              <InputBox />
+              <InputBox
+                label="To"
+                amount={result}
+                currencyOptions={options}
+                selectCurrency={toValue}
+                onCurrencyChange={(currency) => setToValue(currency)}
+                amountDisable
+              />
             </div>
             <button
               type="submit"
               className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg hover:bg-blue-500"
             >
-              Convert
+              Convert {fromValue.toUpperCase()} to {toValue.toUpperCase()}
             </button>
           </form>
         </div>
